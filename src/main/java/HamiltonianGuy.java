bd4014ebbe3288a55681bdcecf36a967e097115b
P0LLUX26\Sivar <sivaram260202@gmail.com>
2025-01-23 21:49:57 +0800

    Level-1: Echo

--- src/main/java/HamiltonianGuy.java
+++ src/main/java/HamiltonianGuy.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class HamiltonianGuy {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("______________________________________");
         System.out.println("Hello from I'm HamiltonianGuy");
         System.out.println("What can I do for you?");
         System.out.println("______________________________________");
-        System.out.println("Bye. Hope to see you again.");
-
+        while (true) {
+            String s = sc.nextLine();
+            if (s.equals("bye")) {
+                break;
+            } else {
+                System.out.println("      " + s);
+            }
+        }
+        System.out.println("      Bye. Hope to see you again.");
+        sc.close();
     }
 }
