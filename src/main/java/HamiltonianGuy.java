cae0e68ff69fdc6fc39db982d8d4df7f27a71780
P0LLUX26\Sivar <sivaram260202@gmail.com>
2025-01-23 21:28:59 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/HamiltonianGuy.java
@@ -0,0 +1,10 @@
+public class HamiltonianGuy {
+    public static void main(String[] args) {
+        System.out.println("______________________________________");
+        System.out.println("Hello from I'm HamiltonianGuy");
+        System.out.println("What can I do for you?");
+        System.out.println("______________________________________");
+        System.out.println("Bye. Hope to see you again.");
+
+    }
+}
